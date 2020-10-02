02d5d61250182861a0756a2887ee5286ed3c952d
Suranjana <suranjana1128@gmail.com>
2020-10-02 22:22:35 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+var item_button = document.getElementsByClassName("item");
+
+item_button[0].addEventListener("click", function() {
+    console.log("Clicked on profile")
+})
+
+item_button[1].addEventListener("click", function() {
+    console.log("Clicked on stats")
+})
+
+item_button[2].addEventListener("click", function() {
+    console.log("Clicked on feed")
+})
