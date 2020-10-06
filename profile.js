8da2d47703a2d4f880ccc16fad4271cc0cf16800
Suranjana <suranjana1128@gmail.com>
2020-10-06 23:02:31 +0800

    Refactoring some repeated code.

--- profile.js
+++ profile.js
@@ -1,38 +1,25 @@
 var list_button = document.getElementsByTagName("button")[0];
 list_button.addEventListener("click", function() {
-    console.log("Hit button!");
-    var next_read = prompt("Enter a name for your next read:");
-    console.log(next_read);
-
-    list_item = document.createElement("li");
-    list_item.appendChild(document.createTextNode(next_read));
-    
-    ongoing_list = document.getElementsByTagName("ol")[0];
-    ongoing_list.appendChild(list_item);
+    addToList(0);
 })
 
 var list_button = document.getElementsByTagName("button")[1];
 list_button.addEventListener("click", function() {
-    console.log("Hit button!");
-    var next_read = prompt("Enter a name for your next read:");
-    console.log(next_read);
-
-    list_item = document.createElement("li");
-    list_item.appendChild(document.createTextNode(next_read));
-    
-    ongoing_list = document.getElementsByTagName("ol")[1];
-    ongoing_list.appendChild(list_item);
+    addToList(1);
 })
 
 var list_button = document.getElementsByTagName("button")[2];
 list_button.addEventListener("click", function() {
-    console.log("Hit button!");
+    addToList(2);
+})
+
+function addToList(index) {
     var next_read = prompt("Enter a name for your next read:");
     console.log(next_read);
 
     list_item = document.createElement("li");
     list_item.appendChild(document.createTextNode(next_read));
     
-    ongoing_list = document.getElementsByTagName("ol")[2];
+    ongoing_list = document.getElementsByTagName("ol")[index];
     ongoing_list.appendChild(list_item);
-})
+}
